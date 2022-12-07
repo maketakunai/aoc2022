b53ade69bdc622b8dc9628ab6f21515fe5060da5
maketakunai <timvan@berkeley.edu>
2022-12-06 23:19:11 -0800

    day 7

---
+++ python/07b.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def sum_file_sizes(dir):
+    return sum([n[1] for n in dir])
+
+with open('./input/07_input.txt') as input_file:
+    cur_dir = []
+    files = {}
+    for line in input_file.read().splitlines():
+        if line[0] == '$':
+            command = line.split(' ')
+            if command[1] == 'cd' and command[2] != '..':
+                cur_dir.append(command[2])
+            elif command[1] == 'cd' and command[2] == '..':
+                cur_dir.pop() 
+        else:
+            ls_result = line.split(' ')
+            if ls_result[0] != 'dir':
+                f_size = int(ls_result[0])
+                for n in range(len(cur_dir)):
+                    if tuple(cur_dir[:n+1]) not in files:
+                        files[tuple(cur_dir[:n+1])] = f_size
+                    else:
+                        files[tuple(cur_dir[:n+1])] += f_size
+
+    tot_space = 70000000
+    root_space = files[('/',)]
+    unused_space = tot_space - root_space
+    req_space = 30000000 - unused_space
+    
+    smallest_dir = min(file_size for file_size in files.values() if file_size >= req_space)
+    print(smallest_dir)
